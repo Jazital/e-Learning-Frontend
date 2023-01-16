74e7311221a997ff8a3bc713955c9ed1194ced6d
Nelson Ogueh <nelsonovovwe@gmail.com>
2023-01-16 13:03:42 +0100

    Fix: Minor fixes

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,31 +1,27 @@
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 import SideBar from "./SideBar";
-import SideBarLecturer from "../lecturers/nav/SideBarLecturer";
 import NavHader from "./NavHeader";
 import Header from "./Header";
 
 
-const KokiNav = ({title}) => {
-    var userRole = localStorage.getItem('user_role') || ""
-    const [toggle, setToggle] = useState("");
-    const onClick = (name) => setToggle(toggle === name ? "" : name);
-    return (
-        <Fragment>
-            <NavHader />
-            {
-                userRole == "student" && (<SideBar />) || (userRole == "lecturer") && (<SideBarLecturer />)
-            }
 
-            <Header
-                onActivity={() => onClick("activity")}
-                onNotification={() => onClick("notification")}
-                onProfile={() => onClick("profile")}
-                toggle={toggle}
-                title={title}
-            />
-
+const KokiNav = ({ title }) => {
+   const [toggle, setToggle] = useState("");
+   const onClick = (name) => setToggle(toggle === name ? "" : name);
+   return (
+      <Fragment>
+         <NavHader />
+         <SideBar />
+         <Header
+            onActivity={() => onClick("activity")}
+            onNotification={() => onClick("notification")}
+            onProfile={() => onClick("profile")}
+            toggle={toggle}
+            title={title}
+         />
+           
         </Fragment>
-    );
+   );
 };
 
 export default KokiNav;
